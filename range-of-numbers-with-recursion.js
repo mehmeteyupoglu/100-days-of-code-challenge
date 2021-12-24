0ea10ee9163023cc7edad6e2ac774e52159c1e9f
mehmeteyupoglu <mehmeteyupoglu2@gmail.com>
2021-12-24 22:32:45 +0300

    Update

--- range-of-numbers-with-recursion.js
+++ range-of-numbers-with-recursion.js
@@ -1,17 +1,17 @@
 function rangeOfNumbers(startNum, endNum) {
-    if(startNum === endNum){ // base case
-      return [startNum]
+    if (startNum === endNum) { // base case
+        return [startNum]
     }
-  
+
     let numbers = rangeOfNumbers(startNum, endNum - 1)
     numbers.push(endNum)
     return numbers;
-  };
-  
-  console.log(rangeOfNumbers(1, 5))
-  
-  // rangeOfNumbers(1, 5) => [1, 2, 3, 4, 5]
-  // rangeOfNumbers(1, 5) => rangeOfNumbers(1, 4) => [1, 2, 3, 4]
-  // rangeOfNumbers(1, 5) => rangeOfNumbers(1, 4) => rangeOfNumbers(1, 3) => [1, 2, 3]
-  // rangeOfNumbers(1, 5) => rangeOfNumbers(1, 4) => rangeOfNumbers(1, 3) => rangeOfNumbers(1, 2) => [1, 2]
-  // rangeOfNumbers(1, 5) => rangeOfNumbers(1, 4) => rangeOfNumbers(1, 3) => rangeOfNumbers(1, 2) => rangeOfNumbers(1, 1) => [1] 
+};
+
+console.log(rangeOfNumbers(1, 5))
+
+// rangeOfNumbers(1, 5) => [1, 2, 3, 4, 5]
+// rangeOfNumbers(1, 5) => rangeOfNumbers(1, 4) => [1, 2, 3, 4]
+// rangeOfNumbers(1, 5) => rangeOfNumbers(1, 4) => rangeOfNumbers(1, 3) => [1, 2, 3]
+// rangeOfNumbers(1, 5) => rangeOfNumbers(1, 4) => rangeOfNumbers(1, 3) => rangeOfNumbers(1, 2) => [1, 2]
+// rangeOfNumbers(1, 5) => rangeOfNumbers(1, 4) => rangeOfNumbers(1, 3) => rangeOfNumbers(1, 2) => rangeOfNumbers(1, 1) => [1] 
